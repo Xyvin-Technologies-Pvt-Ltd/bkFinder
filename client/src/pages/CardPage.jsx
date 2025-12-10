4aadbf63072493b1d5ee37d792fc5fcd60a94a57
faaizmohammed <faaizbymohammed@gmail.com>
2025-12-10 13:02:37 +0530

    bug fix

--- client/src/pages/CardPage.jsx
+++ client/src/pages/CardPage.jsx
@@ -129,6 +129,7 @@ export default function CardPage() {
                             }}
                         />
                     )}
+                    
 
                     {/* QR */}
                     {user.qr && (
