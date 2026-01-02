5d17236e834d59a19ef2e854a7ab2a742a229f4c
faaizmohammed <faaizbymohammed@gmail.com>
2026-01-02 17:26:36 +0530

    added t&c and privacy policy

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -3,10 +3,14 @@ import ViewExcel from "../pages/ViewExcel.jsx";
 import Homepage from "../pages/Homepage.jsx";
 import Login from "../pages/Login.jsx";
 import CardPage from "../pages/CardPage.jsx";
+import TermsAndConditions from "../pages/TermsAndConditions.jsx";
+import PrivacyPolicy from "../pages/PrivacyPolicy.jsx";
 
 export const routes = createBrowserRouter([
   { path: "/", element: <Homepage /> },
   { path: "/login", element: <Login /> }, 
   { path: "/managexcel", element: <ViewExcel /> },
   { path: "/card/:id", element: <CardPage /> },
+  { path: "/terms-and-conditions", element: <TermsAndConditions/>},
+  { path: "/privacy-policy", element: <PrivacyPolicy/> }
 ]);
